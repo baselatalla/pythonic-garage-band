2ae6c5a464846b7987a0d76aef1c49633eca040d
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-06-09 23:16:27 +0300

    still work

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,33 @@
+class Band():
+    def __init__(self,name,members):
+        self.name = name
+        self.member = members
+    
+    def play_solos():
+        solos = []
+
+        return solos
+
+    pass
+
+
+
+class Musician(Band):
+    def __repr__(self):
+        pass
+    def __str__(self):
+        pass
+    pass
+    def get_instrument(self):
+        pass
+    def play_solo(self):
+        pass
+
+
+class Guitarist(Musician ):
+    pass
+class  Bassist(Musician ):
+    pass
+class Drummer(Musician ):
+    pass
+
